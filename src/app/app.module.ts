7c6c47603a7c52eabe84cf01c74ef9a19fb30f10
jhallick <jhallick@jhallick-mbp.local>
2019-10-24 15:48:07 -0400

    feat(xstate): Initial test code push to display xstate issue with angular sourcemaps;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './components/default/default.component';
+import { TestMachine } from './components/default/+xstate/machine.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TestMachine],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/default/+xstate/machine.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Machine, interpret } from 'xstate';
+
+@Injectable()
+export class TestMachine {
+    private machine = Machine({
+        id: 'test',
+        initial: 'boot',
+        states: {
+            boot: {
+                on: {
+                    '': 'final'
+                }
+            },
+            final: {
+                type: 'final',
+                entry: () => console.log('in final state!')
+            }
+        }
+    });
+    private service = interpret(this.machine, { devTools: true }).start();
+
+    send(event) {
+        this.service.send(event);
+    }
+
+    constructor() {}
+}
